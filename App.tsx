33e504242746254c14039e3bbd699f9379c69433
Elijah Guarina <elijah.guarina@gmail.com>
2021-09-06 10:33:02 +1200

    LRF App Creation

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
