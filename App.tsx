8454ccd5cd125a588cafa4eb3e2aa7f40d55d9dc
Elijah Guarina <elijah.guarina@gmail.com>
2021-10-04 13:59:10 +1300

    Set up a bit of All_Ingredients, still need to add Firebase and other functions

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import IngredientSelection from "./pages/MainIngredientSelect";
 import SelectedIngredientsDisplayScreen from './pages/SelectedIngredientsDisplay';
 import RecipeResults from "./pages/RecipeResults";
 // States
+import AllIngredientsProvider from './states/All_Ingredients';
 import IngredientsListProvider from './states/SelectedIngredientsList';
 // Styles for divs
 import "./styles/styles.css"
@@ -14,21 +15,22 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-
-    <IngredientsListProvider>
-      <NavigationContainer>
-        <Stack.Navigator 
-          initialRouteName="Ingredient Selection"
-          screenOptions={{
-            headerShown: false
-          }}
-        >
-          
-          <Stack.Screen name="Ingredient Selection" component={IngredientSelection} />
-          <Stack.Screen name="Selected Ingredients" component={SelectedIngredientsDisplayScreen} />
-          
-        </Stack.Navigator>
-      </NavigationContainer>
-    </IngredientsListProvider>
+    <AllIngredientsProvider>
+      <IngredientsListProvider>
+        <NavigationContainer>
+          <Stack.Navigator 
+            initialRouteName="Ingredient Selection"
+            screenOptions={{
+              headerShown: false
+            }}
+          >
+            
+            <Stack.Screen name="Ingredient Selection" component={IngredientSelection} />
+            <Stack.Screen name="Selected Ingredients" component={SelectedIngredientsDisplayScreen} />
+            
+          </Stack.Navigator>
+        </NavigationContainer>
+      </IngredientsListProvider>
+    </AllIngredientsProvider>
   );
 }
--- states/All_Ingredients.tsx
+++ states/All_Ingredients.tsx
@@ -1,16 +1,37 @@
 // 3rd-party Imports
 import React, { useEffect, useMemo, useState } from 'react'
 
-export default function IngredientsListProvider({ children }) {
-    const [allIngredients, setAllIngredients] = useState<string[]>([]);
+function ingredientsMapDefaultValue() : Map<string,string[]> {
+    var map = new Map<string,string[]>();
+    map.set("Meat", ["Beef", "Pork", "Chicken"]);
+    map.set("Vegetables", ["Potato", "Tomato", "Carrot"]);
+    map.set("Fruits", ["Apple", "Banana", "Orange"]);
+    return map;
+}
 
-    const searchedIngredientsProviderValue = useMemo(() => ({searchedIngredients,setSearchedIngredients}), [searchedIngredients,setSearchedIngredients]);
+const searchbarTextInputStateDefaultValue = {
+    allIngredients: ingredientsMapDefaultValue(),
+    setAllIngredients: (state:Map<string,string[]>) => {}
+};
 
-    function updateAndRefreshAllIngredients({ingredientsList,updateIngredientsList}) {
+export const AllIngredients = React.createContext(searchbarTextInputStateDefaultValue);
+
+export default function AllIngredientsProvider({ children }) {
+    const [allIngredients, setAllIngredients] = useState<Map<string,string[]>>(searchbarTextInputStateDefaultValue);
+
+    const allIngredientsProviderValue = useMemo(() => ({allIngredients,setAllIngredients}), [allIngredients,setAllIngredients]);
+
+    function updateAndRefreshAllIngredients({allIngredients,setAllIngredients}) {
         //let new
     }
 
+    useEffect(() => {
+        console.log("All_Ingredients useEffect", allIngredients);
+    }, [allIngredients]);
+
     return (
-        
+        <AllIngredients.Provider value={allIngredientsProviderValue}>
+            {children}
+        </AllIngredients.Provider>
     );
 }
